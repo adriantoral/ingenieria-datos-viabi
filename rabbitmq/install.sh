2ea9a4df6a3d6c0b463442bae2529d6733b0ab51
adriantoral <adrian.rubiboy11@gmail.com>
2025-02-20 17:36:34 +0100

    feat: actualiza rabbitmq para usar modo cluster
    Closes VIABI-36

---
+++ rabbitmq/install.sh
@@ -0,0 +1,6 @@
+# Instalar RabbitMQ Cluster Operator
+kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+
+# Verificar que el operador se haya instalado correctamente
+kubectl get all -n rabbitmq-system
+kubectl get customresourcedefinitions.apiextensions.k8s.io
